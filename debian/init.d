9fac86a08bc1b0722737f220a5a0297d3a4e4cb1
guillem <guillem@50eb543b-83fa-0310-9bb7-f7cbbee4ab39>
2008-12-19 13:09:13 +0000

    Revert "Export USER, LOGNAME, SHELL and HOME to maemo-launcher"
    This reverts commit 253d2db0977bcfa0aeca0cce09c822162523ff6e,
    commit 498d8ebc1376a60423e50a722d9db6656b8d2bba
    and commit 3822a83524ff2f44e2ed46104558ca51f3d188dc.
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/maemo-launcher@17016 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- debian/init.d
+++ debian/init.d
@@ -3,8 +3,6 @@
 # $Id$
 #
 
-set -e
-
 NAME=maemo-launcher
 DESC="Maemo Launcher"
 DAEMON=/usr/bin/$NAME
@@ -13,7 +11,26 @@ DAEMON=/usr/bin/$NAME
 DAEMON_BASE_OPTS="--daemon --booster gtk"
 PIDFILE=/tmp/$NAME.pid
 
+# When inside scratchbox we are not really root nor do we have 'user' user
+if [ -f /targets/links/scratchbox.config ]; then
+  DAEMON_OPTS="$DAEMON_BASE_OPTS"
+else
+  # FIXME: this is wrong wrong wrong, and should not be hardcoded, this script
+  #        belongs in the session level instead.
+  PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+  USER=user
+  HOME=/home/$USER
+  if [ `id -u` = 0 ]; then
+    CHUID="--chuid $USER"
+    # Make sure the launcher is not an OOM candidate
+    NICE="--nicelevel -1"
+  fi
+  DAEMON_OPTS="$DAEMON_BASE_OPTS --quiet"
+fi
+
 test -x $DAEMON || exit 0
+
+set -e
 
 # Those files set needed environment variables for the Maemo applications
 # FIXME: but this should not be needed either, and should be inherited from
@@ -28,26 +45,6 @@ then
 else
   echo "$DEFSDIR/af-defines.sh not found!"
   exit 1
-fi
-
-# When inside scratchbox we are not really root nor do we have 'user' user
-if [ -f /targets/links/scratchbox.config ]; then
-  DAEMON_OPTS="$DAEMON_BASE_OPTS"
-else
-  # FIXME: this is wrong wrong wrong, and should not be hardcoded, this script
-  #        belongs in the session level instead.
-  PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-  export USER=user
-  export LOGNAME=$USER
-  PWENTRY=`getent passwd $USER`
-  export HOME=`echo $PWENTRY | cut -d: -f6`
-  export SHELL=`echo $PWENTRY | cut -d: -f7`
-  if [ `id -u` = 0 ]; then
-    CHUID="--chuid $USER"
-    # Make sure the launcher is not an OOM candidate
-    NICE="--nicelevel -1"
-  fi
-  DAEMON_OPTS="$DAEMON_BASE_OPTS --quiet"
 fi
 
 case "$1" in
